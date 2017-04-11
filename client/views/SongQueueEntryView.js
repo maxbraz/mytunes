2bd5d984ab4df67262084dceb27b3d36d2a579c3
Hack Reactor Student <student@hackreactor.com>
2017-04-11 12:34:04 -0700

    passing lib entry view specs

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,18 +1,12 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
- tagName: 'tr',
+  tagName: 'tr',
 
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
 
   events: {
     'click': function() {
-      // if(this.collection.at(0)) {
-        this.model.enqueue();
-      // } else {
-        this.model.play();
-      // }
-      //***************************    HALP DESK   ******************************************************
-      //should this file look to see if there is something in the queueu and either play or enqueue?
+      this.model.play();
     }
   },
 
