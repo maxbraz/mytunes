7936fd614f8028ea73566c10b236ee9aba2555fa
Hack Reactor Student <student@hackreactor.com>
2017-04-11 20:13:12 -0700

    pass songs parse specs

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,5 +1,24 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
+  
+  initialize: function(params) {
+    this.fetch({
+      success: function(){
+        console.log('Max is beans');
+      },
+      error: function(e){
+        console.log('Max is a brazzzzzzillion', e);
+      },
+      complete: function(e){
+        console.log('Fetch complete', e);
+      }
+    });
+  },
+  'url': 'http://parse.sfm6.hackreactor.com/mytunes/classes/songs',
+
+  parse: function(response) {
+    return response.results;
+  },
 
   model: SongModel
 
